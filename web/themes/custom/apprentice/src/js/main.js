996c2e04bc552e3d7d7d6a88de82830b684d45c0
Natalie Groeneboom <nataliegroeneboom@Natalies-Air.connect>
2018-06-07 16:00:31 +0100

    chage to about me page

--- web/themes/custom/apprentice/src/js/main.js
+++ web/themes/custom/apprentice/src/js/main.js
@@ -3,20 +3,46 @@
 	Drupal.behaviors.myModuleBehavior = {
 		attach:function(context, settings){
 
-	
+
 			$(".topics-covered.zebra-odd").addClass("col-md-4 col-md-offset-2");
 			$(".topics-covered.zebra-even").addClass("col-md-4 col-md-offset-1");
-			
+			$("#block-apprentice-main-menu").addClass("navbar");
 
-			$(".blog-grid").mouseover(function() {
-         $(this).animate({ backgroundColor:'rgb(0,128,0,0.2)'},1000);
-    }).mouseout(function() {
-        $(this).animate({ backgroundColor:'rgb(0,128,0,0)'},1000);
-    });  
 
-		}			
 
-			
+
+function navBar(){
+	var collapse = document.getElementById("block-apprentice-main-menu");
+
+	 if(collapse.className === "navbar"){
+		 	collapse.className += " responsive";
+	 }else{
+		 collapse.className === "navbar";
+	 }
+
+}
+
+
+	$(".navbar > .main-menu").once().append('<li class="icon"><a href="javascript:void(0);" ><i class="fa fa-bars"></i></a></li>');
+
+
+
+$('.icon').once().click(function(){
+	$('.main-menu').toggleClass('responsive');
+
+})
+
+
+
+
+
+
+
+
+
+} //end of drupal jquery
+
+
 
 	}
 
